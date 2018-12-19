9c75b262df5f1856fc9327eeb23d405c54de9ee8
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2018-12-19 01:05:54 -0700

    Create Array_question3.java

---
+++ Array_question3.java
@@ -0,0 +1,130 @@
+/*
+You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+
+For example, given the array  we perform the following steps:
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+It took  swaps to sort the array.
+
+Function Description
+
+Complete the function minimumSwaps in the editor below. It must return an integer representing the minimum number of swaps to sort the array.
+
+minimumSwaps has the following parameter(s):
+
+arr: an unordered array of integers
+Input Format
+
+The first line contains an integer, , the size of . 
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Return the minimum number of swaps to sort the given array.
+
+Sample Input 0
+
+4
+4 3 1 2
+Sample Output 0
+
+3
+Explanation 0
+
+Given array  
+After swapping  we get  
+After swapping  we get  
+After swapping  we get  
+So, we need a minimum of  swaps to sort the array in ascending order.
+
+Sample Input 1
+
+5
+2 3 4 1 5
+Sample Output 1
+
+3
+Explanation 1
+
+Given array  
+After swapping  we get  
+After swapping  we get  
+After swapping  we get  
+So, we need a minimum of  swaps to sort the array in ascending order.
+
+Sample Input 2
+
+7
+1 3 5 2 4 6 7
+Sample Output 2
+
+3
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int swap = 0;
+        boolean[] visited = new boolean[arr.length];
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            int j = i;
+            int cycle = 0;
+            while(!visited[j])
+            {
+                visited[j] = true;
+                j = arr[j] - 1;
+                cycle++;
+            }
+            if(cycle != 0)
+            {
+              swap+= cycle - 1;  
+            }
+        }
+        return swap;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
