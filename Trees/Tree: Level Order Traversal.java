ea1483cf3e24bdf639fbc256982c9d726af9aac2
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2018-12-22 21:31:24 -0700

    Update Tree: Level Order Traversal.java

--- Trees/Tree: Level Order Traversal.java
+++ Trees/Tree: Level Order Traversal.java
@@ -1,13 +1,21 @@
+import java.util.*;
+import java.io.*;
 
-
-	/* 
+class Node {
+    Node left;
+    Node right;
+    int data;
     
-    class Node 
-    	int data;
-    	Node left;
-    	Node right;
-	*/
-	public static void levelOrder(Node root) {
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+ public static void levelOrder(Node root) {
       if(root == null) return;
       Queue<Node> qr = new LinkedList<>();
       qr.add(root);
@@ -23,7 +31,35 @@
           {
                qr.add(d.right);
           }
-      }
-      
+      }      
     }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}
 
