8be74f13de78eb91f390b03849724ee0bf9ad285
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2018-12-22 21:33:25 -0700

    Update Tree: Preorder Traversal.java

--- Trees/Tree: Preorder Traversal.java
+++ Trees/Tree: Preorder Traversal.java
@@ -1,17 +1,21 @@
-
-
-/* you only have to complete the function given below.  
-Node is defined as  
+import java.util.*;
+import java.io.*;
 
 class Node {
-    int data;
     Node left;
     Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
 
-*/
+class Solution {
 
-    public static void preOrder(Node root) 
+   public static void preOrder(Node root) 
     {
         if(root == null) return;
         System.out.print(root.data + " ");
@@ -19,3 +23,32 @@ class Node {
         preOrder(root.right);
     }
 
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }	
+}
