56c5e222100b20e79b078da44b773e0fc2e8a15b
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2019-11-12 20:28:34 -0700

    Create Print_the_Elements_of_a_Linked_List.java

---
+++ Data Structures/Linked Lists/Print_the_Elements_of_a_Linked_List.java
@@ -0,0 +1,7 @@
+    static void printLinkedList(SinglyLinkedListNode head) {
+        SinglyLinkedListNode temp = head;
+        while(temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
